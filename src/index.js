5aeec2eab5dc16717ebee7278a61054c3c01e5e3
Beatriz Fernandes <93296323+Trizferoli@users.noreply.github.com>
2022-08-15 21:39:13 -0300

    deleted debug console.log
    deleted debug console.log

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const routes = require('./routes');
 
 require('dotenv').config()
-console.log(process.env.DB_CLIENT)
+
 app.use(express.json());
 app.use(routes);
 app.listen(process.env.PORT || 3000);
