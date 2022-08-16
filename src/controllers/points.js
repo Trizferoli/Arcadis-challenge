599dd431ef0758909acc7ad576ed065849c94084
Beatriz Fernandes <93296323+Trizferoli@users.noreply.github.com>
2022-08-15 21:37:32 -0300

    deleted comments
    deleted comments

--- src/controllers/points.js
+++ src/controllers/points.js
@@ -1,8 +1,5 @@
 const schemaAddPoints = require('../validation/addPoint');
 const knex = require('../connection')
-// const pointExists = require('./functions/pointExists');
-
-
 
 const addPoint = async (req, res) => {
     const { x, y } = req.body
@@ -49,37 +46,11 @@ const getPoint = async (req, res) => {
 
 const getAllPoints = async (req, res) => {
     try {
-        // const points = await knex.raw('SELECT id_ponto, data_coleta, valor_parametro, pontos.ponto_x, pontos.ponto_y, parametros_limite.nome, parametros_limite.unidade_de_medida FROM parametros_registro JOIN parametros_limite ON id_parametros_limite = parametros_limite.id  FULL JOIN pontos ON id_ponto = pontos.id GROUP BY parametros_registro.id_ponto');
-
-        // const points = await knex.raw('SELECT pontos.id, parametros_registro.data_coleta, parametros_registro.valor_parametro, pontos.ponto_x, pontos.ponto_y, parametros_limite.nome, parametros_limite.unidade_de_medida FROM parametros_registro JOIN parametros_limite ON id_parametros_limite = parametros_limite.id  FULL JOIN pontos ON id_ponto = pontos.id GROUP BY pontos.id');
-
-        // const points = await knex('parametros_registro')
-        //     .fullOuterJoin('pontos', 'id_ponto', '=', 'pontos.id')
-        //     .join('parametros_limite', 'id_parametros_limite', '=', 'parametros_limite.id')
-        //     .select('id_ponto', 'data_coleta', 'valor_parametro', 'pontos.ponto_x', 'pontos.ponto_y', 'parametros_limite.nome', 'parametros_limite.unidade_de_medida').groupBy('id_ponto')
-
-
-        //FIX GROUP BY
-        // const points = await knex('pontos')
-        //     .join('parametros_limite', function () {
-        //         this
-        //             .on('parametros_registro.id_parametros_limite', '=', 'parametros_limite.id')
-        //     }).join('parametros_regisstro', function () {
-        //         this
-        //             .on('pontos.id', '=', 'parametros_registro.id_ponto')
-        //     })
-        //     .select('pontos.id', 'parametros_registro.data_coleta', 'parametros_registro.valor_parametro', 'pontos.ponto_x', 'pontos.ponto_y', 'parametros_limite.nome', 'parametros_limite.unidade_de_medida').groupBy('pontos.id').orderBy('pontos.id')
-
+        
         const points = await knex('pontos')
             .fullOuterJoin('parametros_registro', 'pontos.id', '=', 'parametros_registro.id_ponto')
             .leftJoin('parametros_limite', 'parametros_registro.id_parametros_limite', '=', 'parametros_limite.id').as('t1')
             .select('pontos.id', 'parametros_registro.data_coleta', 'parametros_registro.valor_parametro', 'pontos.ponto_x', 'pontos.ponto_y', 'parametros_limite.nome', 'parametros_limite.unidade_de_medida').groupBy('parametros_registro.data_coleta').groupBy('parametros_registro.valor_parametro').groupBy('parametros_limite.nome').groupBy('parametros_limite.unidade_de_medida').groupBy('pontos.id').orderBy('pontos.id')
-
-        // const points = await knex('parametros_registro')
-        //     .fullOuterJoin('pontos', 'id_ponto', '=', 'pontos.id')
-        //     .join('parametros_limite', 'id_parametros_limite', '=', 'parametros_limite.id').as('parametros_limite_id')
-        //     .select('pontos.id', 'parametros_registro.data_coleta', 'parametros_registro.valor_parametro', 'pontos.ponto_x', 'pontos.ponto_y', 'parametros_limite.nome', 'parametros_limite.unidade_de_medida').groupBy('parametros_registro.data_coleta').groupBy('parametros_registro.valor_parametro').groupBy('parametros_limite.nome').groupBy('parametros_limite.unidade_de_medida').groupBy('pontos.id').orderBy('pontos.id')
-
 
         return res.status(200).json(points)
     } catch (error) {
