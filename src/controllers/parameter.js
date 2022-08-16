fcec071f0e3abb39939a57e07edf57f349c06cbf
Beatriz Fernandes <93296323+Trizferoli@users.noreply.github.com>
2022-08-15 21:35:39 -0300

    deleted comments
    deleted comments

--- src/controllers/parameter.js
+++ src/controllers/parameter.js
@@ -61,16 +61,7 @@ const getParameter = async (req, res) => {
 
 const getIrregularParameters = async (req, res) => {
     try {
-        // const point = await knex('pontos')
-        //     .fullOuterJoin('parametros_registro', 'pontos.id', '=', 'parametros_registro.id_ponto')
-        //     .leftJoin('parametros_limite', 'parametros_rsegistro.id_parametros_limite', '=', 'parametros_limite.id')
-        //     .select('pontos.id', 'parametros_registro.data_coleta', 'parametros_registro.valor_parametro', 'pontos.ponto_x', 'pontos.ponto_y', 'parametros_limite.nome', 'parametros_limite.unidade_de_medida').where('parametros.registro.valor_parametro', '>=', 'parametros_limite.valor_limites').groupBy('parametros_registro.data_coleta').groupBy('parametros_registro.valor_parametro').groupBy('parametros_limite.nome').groupBy('parametros_limite.unidade_de_medida').groupBy('pontos.id').orderBy('pontos.id')
         const point = await knex.raw('SELECT pontos.id, parametros_registro.data_coleta, parametros_registro.valor_parametro, pontos.ponto_x, pontos.ponto_y, parametros_limite.nome, parametros_limite.unidade_de_medida FROM parametros_registro JOIN parametros_limite ON id_parametros_limite = parametros_limite.id  FULL JOIN pontos ON id_ponto = pontos.id WHERE parametros_registro.valor_parametro >= parametros_limite.valor_limite GROUP BY pontos.id, parametros_registro.data_coleta, parametros_registro.data_coleta, parametros_registro.valor_parametro, parametros_limite.nome, parametros_limite.unidade_de_medida');
-
-        // const point = await knex('parametros_registro')
-        //     .fullOuterJoin('pontos', 'id_ponto', '=', 'pontos.id')
-        //     .join('parametros_limite', 'id_parametros_limite', '=', 'parametros_limite.id').as('parametros_limite_id')
-        //     .select('pontos.id', 'parametros_registro.data_coleta', 'parametros_registro.valor_parametro', 'pontos.ponto_x', 'pontos.ponto_y', 'parametros_limite.nome', 'parametros_limite.unidade_de_medida').where('parametros_registro.valor_parametro', '>=', 'parametros_limite.valor_limite').groupBy('parametros_registro.data_coleta').groupBy('parametros_registro.valor_parametro').groupBy('parametros_limite.nome').groupBy('parametros_limite.unidade_de_medida').groupBy('pontos.id').orderBy('pontos.id')
 
         return res.status(200).json(point.rows)
     } catch (error) {
