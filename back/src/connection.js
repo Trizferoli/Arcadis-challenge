30cbcc2245de761ae6f1ba8fd70066461f6f8b56
Trizferoli <biaferoi@gmail.com>
2022-08-15 21:05:11 -0300

    teste

--- back/src/connection.js
+++ back/src/connection.js
@@ -12,15 +12,16 @@
 
 // module.exports = knex;
 const knex = require('knex')({
-    client: process.env.DB_CLIENT,
+    // client: process.env.DB_CLIENT,
+    client: 'pg',
     connection: {
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DB_DATABASE,
-        password: process.env.DB_PASSWORD,
-        ssl: {
-            rejectUnauthorized: false
-        }
+        password: process.env.DB_PASSWORD
+        // ssl: {
+        //     rejectUnauthorized: false
+        // }
     }
 
 });
--- back/src/index.js
+++ back/src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-
-require('dotenv').config()
-
 const routes = require('./routes');
 
+require('dotenv').config()
+console.log(process.env.DB_CLIENT)
 app.use(express.json());
 app.use(routes);
 app.listen(process.env.PORT || 3000);
