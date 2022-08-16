0ec879f40176f5a964240e39da7556fe2fef7168
Trizferoli <biaferoi@gmail.com>
2022-08-15 21:13:05 -0300

    deleted back folder

--- back/src/validation/addPoint.js
+++
@@ -1,8 +0,0 @@
-const yup = require('./yupConfig');
-
-const schemaAddPoint = yup.object().shape({
-    x: yup.number().strict().required(),
-    y: yup.number().strict().required()
-})
-
-module.exports = schemaAddPoint;
