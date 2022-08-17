8e63dcfbbbde3649dd0f74e23dc2056af73fc570
Trizferoli <biaferoi@gmail.com>
2022-08-17 12:27:47 -0300

    changed getPoint function return

--- src/controllers/points.js
+++ src/controllers/points.js
@@ -28,14 +28,17 @@ const addPoint = async (req, res) => {
 const getPoint = async (req, res) => {
     const { id } = req.params
     try {
-        const point = await knex('pontos')
-            .fullOuterJoin('parametros_registro', 'pontos.id', '=', 'parametros_registro.id_ponto')
-            .leftJoin('parametros_limite', 'parametros_registro.id_parametros_limite', '=', 'parametros_limite.id')
-            .select('pontos.id', 'parametros_registro.data_coleta', 'parametros_registro.valor_parametro', 'pontos.ponto_x', 'pontos.ponto_y', 'parametros_limite.nome', 'parametros_limite.unidade_de_medida').where('pontos.id', id).groupBy('parametros_registro.data_coleta').groupBy('parametros_registro.valor_parametro').groupBy('parametros_limite.nome').groupBy('parametros_limite.unidade_de_medida').groupBy('pontos.id').orderBy('pontos.id')
+        const point = await knex('pontos').select('*').first();
 
-        if (point.length === 0) {
+        if (!point) {
             return res.status(404).json({ "message": "Este ponto não está cadastrado." })
         }
+
+        const parameters = await knex('parametros_registro')
+            .leftJoin('parametros_limite', 'parametros_registro.id_parametros_limite', '=', 'parametros_limite.id')
+            .select('parametros_registro.data_coleta', 'parametros_registro.valor_parametro', 'parametros_limite.nome', 'parametros_limite.unidade_de_medida').where('parametros_registro.id_ponto', id).groupBy('parametros_registro.data_coleta').groupBy('parametros_registro.valor_parametro').groupBy('parametros_limite.nome').groupBy('parametros_limite.unidade_de_medida').groupBy('parametros_registro.id_ponto').orderBy('parametros_registro.id_ponto');
+
+        point.parameters = [...parameters];
         return res.status(200).json(point)
     } catch (error) {
         return res.status(400).json({ message: error.message });
@@ -46,7 +49,7 @@ const getPoint = async (req, res) => {
 
 const getAllPoints = async (req, res) => {
     try {
-        
+
         const points = await knex('pontos')
             .fullOuterJoin('parametros_registro', 'pontos.id', '=', 'parametros_registro.id_ponto')
             .leftJoin('parametros_limite', 'parametros_registro.id_parametros_limite', '=', 'parametros_limite.id').as('t1')
